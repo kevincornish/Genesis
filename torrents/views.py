ec070adb156381f9401136005989c607f0e2cc21
Kevin Cornish <kevinz_c@live.com>
2021-11-14 21:40:35 +0000

    start work on bencode to read .torrent file

--- torrents/views.py
+++ torrents/views.py
@@ -8,6 +8,8 @@ from torrents.models import Torrent, Category
 from django.contrib.auth import views
 from django.contrib.auth.decorators import login_required
 
+import bencoder
+
 @login_required(login_url="/login/")
 def browse(request):
     if 'category' in request.GET:
@@ -21,20 +23,24 @@ def browse(request):
 
 @login_required(login_url="/login/")
 def upload(request):
+    categories = Category.objects.all()
     if request.method == 'POST':
         fileUploadForm = TorrentForm(request.POST, request.FILES)
-
-        if fileUploadForm.is_valid():
-            torrentForm = fileUploadForm.save()
-            torrentForm.torrent_path = request.FILES['torrent_path']
-            torrentForm.uploaded_by = request.user.username
-            torrentForm.name = request.POST['name']
-            torrentForm.category = Category.objects.get(id = request.POST['category'])
-            torrentForm.save()
-            return redirect('browse')
+        if request.FILES['torrent_path'].name.lower().endswith(('.torrent')):
+            if fileUploadForm.is_valid():
+                torrentForm = fileUploadForm.save(commit=False)
+                torrentForm.torrent_path = request.FILES['torrent_path']
+                torrentForm.uploaded_by = request.user.username
+                torrentForm.name = request.POST['name']
+                torrentForm.category = Category.objects.get(id = request.POST['category'])
+                torrentForm.save()
+                #lets decode the torrent file and grab what we need    
+                with open(torrentForm.torrent_path.path, 'rb') as torrentfile:
+                    torrent_bytes = bencoder.decode(torrentfile.read())
+                    #print (torrent_bytes)
+                return redirect('browse')
         else:
             return HttpResponse("form is not valid")
     else:
         fileUploadForm = TorrentForm()
-        categories = Category.objects.all()
     return render(request, 'torrents/upload.html', {'form': fileUploadForm,'categories':categories})
