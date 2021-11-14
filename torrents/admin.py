1c08ca2c2c7a86356e1f3418485168c87ea4f98a
Kevin Cornish <kevinz_c@live.com>
2021-11-14 21:42:02 +0000

    use classes for torrent admin

--- torrents/admin.py
+++ torrents/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Torrent,Category
 
-admin.site.register(Category)
-admin.site.register(Torrent)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+admin.site.register( Category, CategoryAdmin)
+
+class TorrentAdmin(admin.ModelAdmin):
+    list_display = ['name','category']
+
+admin.site.register( Torrent, TorrentAdmin)
