2c5c5760a87d0bf4ffb7105b82b7ac0ab2973c74
Kevin Cornish <kevinz_c@live.com>
2021-11-14 22:10:13 +0000

    rename torrent on upload

--- torrents/models.py
+++ torrents/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import uuid
+import os
 
 class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
@@ -13,11 +15,15 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('torrents/', filename)
 
 class Torrent(models.Model):
     name = models.CharField(max_length=255, unique=True, db_index=True, blank=True)
     uploaded_by = models.CharField(max_length=25)
-    torrent_path = models.FileField(upload_to='torrents/')
+    torrent_path = models.FileField(upload_to=get_file_path)
     uploaded_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
     category = models.ForeignKey(Category, verbose_name="Category", on_delete=models.CASCADE, blank=True, null=True)
